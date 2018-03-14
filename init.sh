90ea71d7df2fdd79630bab15ab3a4dd1deff4ee9
alxvgt <1221867+alxvgt@users.noreply.github.com>
2018-03-14 20:20:30 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@
 apt-get install -y sudo vim zsh git curl wget fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s $(which zsh)
+echo "source ~/.custom-zsh.sh" >> ~/.zshrc
+
