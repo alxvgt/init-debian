4558c1fc9cec19a34b1454a8c64bd80d6d29f217
alxvgt <1221867+alxvgt@users.noreply.github.com>
2018-03-14 20:52:07 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "=== Removing previous installation ... === \n"
+echo -e "=== Removing previous installation ... === \n"
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get purge docker-ce
 sudo rm /usr/local/bin/docker-compose
 
-echo "=== Installing ... === \n"
+echo -e "=== Installing ... === \n"
 sudo apt-get update
 sudo apt-get install \
      apt-transport-https \
@@ -23,7 +23,7 @@ sudo apt-get install docker-ce
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo "=== Test ... === \n"
+echo -e "=== Test ... === \n"
 sudo docker --version
 docker-compose --version
-echo "You can test install with : 'sudo docker run hello-world'"
+echo -e "You can test install with : 'sudo docker run hello-world'"
