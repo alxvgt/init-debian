6bc44655d8e0348253af918fe69ca17be03efb19
alxvgt <1221867+alxvgt@users.noreply.github.com>
2018-03-16 21:00:04 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 apt-get install -y sudo vim zsh git curl wget fonts-powerline mlocate
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s $(which zsh)
-sed "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/"
+sed "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/" -i ~/.zshrc
 cp .custom-zsh.sh ~/.custom-zsh.sh
 echo "source ~/.custom-zsh.sh" >> ~/.zshrc
 updatedb &
