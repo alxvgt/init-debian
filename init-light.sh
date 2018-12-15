4e031f90a30bdce087d2b93ecb8babe12214e625
alxvgt <1221867+alxvgt@users.noreply.github.com>
2018-12-15 17:10:56 +0100

    Update init-light.sh

--- init-light.sh
+++ init-light.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#to use this file without git clone, use command below
+#to use this file without git clone, use command below (you must be in root)
 #wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-light.sh | bash
 
-su
 apt-get update && apt-get install -y sudo vim zsh git curl wget fonts-powerline mlocate open-vm-tools open-vm-tools-desktop
 echo "Now, a reboot can be helpful ;-)"
