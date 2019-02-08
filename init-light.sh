7c86677a35cb9703aa1a11a65e7f9740185e896f
alxvgt <1221867+alxvgt@users.noreply.github.com>
2019-02-08 15:59:31 +0100

    Update init-light.sh

--- init-light.sh
+++ init-light.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#to use this file without git clone, use command below (you must be in root)
-#wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-light.sh --no-cache | bash
+# to use this file without git clone, use command below (you must be in root)
+# apt-get update && apt-get install -y wget && wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-light.sh --no-cache | bash
 
 apt-get update && apt-get install -y sudo vim git curl wget mlocate netcat
