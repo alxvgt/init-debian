2eae462302a460ae45592913f67c826a8048f3f8
alxvgt <1221867+alxvgt@users.noreply.github.com>
2019-02-08 14:59:01 +0100

    Update init-zsh.sh

--- init-zsh.sh
+++ init-zsh.sh
@@ -3,9 +3,22 @@
 apt-get install -y zsh fonts-powerline
 zsh --version
 chsh -s $(which zsh)
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/" -i ~/.zshrc
-cp .custom-zsh.sh ~/.custom-zsh.sh
-echo "source ~/.custom-zsh.sh" >> ~/.zshrc
-updatedb &
+
+echo "
+plugins=(
+  git
+  dotenv
+  composer
+  common-aliases
+  docker
+  docker-compose
+  man
+  sudo
+  symfony
+  symfony2
+  vim-interaction
+)" >> ~/.zshrc
 
