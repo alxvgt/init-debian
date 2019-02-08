9f71c9de4c3e425173a7bc66468736718cf86f11
alxvgt <1221867+alxvgt@users.noreply.github.com>
2019-02-08 14:57:20 +0100

    Update and rename init.sh to init-zsh.sh

--- init-zsh.sh
+++ init-zsh.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-apt-get install -y sudo vim zsh git curl wget fonts-powerline mlocate
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+apt-get install -y zsh fonts-powerline
+zsh --version
 chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/" -i ~/.zshrc
 cp .custom-zsh.sh ~/.custom-zsh.sh
 echo "source ~/.custom-zsh.sh" >> ~/.zshrc
