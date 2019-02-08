ed3aaa6db3d1c9f5fbcc01f47787ca3d4f578d0b
alxvgt <1221867+alxvgt@users.noreply.github.com>
2019-02-08 16:19:35 +0100

    Update init-zsh.sh

--- init-zsh.sh
+++ init-zsh.sh
@@ -3,9 +3,17 @@
 #to use this file without git clone, use command below (you must be in root)
 #wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-zsh.sh --no-cache | bash
 
-apt-get install -y zsh fonts-powerline
+sudo apt-get install -y zsh fonts-powerline
 zsh --version
 chsh -s $(which zsh)
+
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.local/share/fonts/
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+fc-cache -vf ~/.local/share/fonts/
+mkdir -p ~/.config/fontconfig/conf.d/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/" -i ~/.zshrc
@@ -25,3 +33,4 @@ plugins=(
   vim-interaction
 )" >> ~/.zshrc
 
+zsh -1
