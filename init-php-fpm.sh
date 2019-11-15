595c2228ee26327fd9093d894ae259dd6bbe30e5
avingtier <avingtier@gmail.com>
2019-11-15 11:30:55 +0100

    some fixes

--- init-php-fpm.sh
+++ init-php-fpm.sh
@@ -3,5 +3,5 @@
 # to use this file without git clone, use command below (you must be in root)
 # apt-get update && apt-get install -y wget && wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-php-fpm.sh --no-cache | bash
 
-apt-get install php-fpm
+apt-get install -y php-fpm
 php -v
