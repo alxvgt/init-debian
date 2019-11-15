301246475495c6817e2305c7d5a62f7b64ab9a0f
avingtier <avingtier@gmail.com>
2019-11-15 12:07:42 +0100

    add php extensions installation script

---
+++ init-php-ext.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# to use this file without git clone, use command below (you must be in root)
+# apt-get update && apt-get install -y wget && wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-php-ext.sh --no-cache | bash
+
+apt-get install -y php-xml php-dom php-mbstring php-intl php-mysql php7.3-sqlite
--- init-symfony-cli.sh
+++ init-symfony-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# to use this file without git clone, use command below (you must be in root)
-# apt-get update && apt-get install -y wget && wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-symfony-cli.sh --no-cache | bash
+# to use this file without git clone, use command below
+# wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-symfony-cli.sh --no-cache | bash
 
 wget https://get.symfony.com/cli/installer -O - | bash
