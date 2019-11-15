b6eb67c593798f09742bb83b7b392f686a368dc4
avingtier <avingtier@gmail.com>
2019-11-15 13:20:23 +0100

    improve sudo installation instructions

--- init-light.sh
+++ init-light.sh
@@ -7,3 +7,6 @@ apt-get update && apt-get install -y sudo vim git curl wget mlocate netcat net-t
 apt-get install -y locales && rm -rf /var/lib/apt/lists/* \
     && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 LANG=en_US.utf8
+
+echo "To add a user to 'sudo' group, use : /usr/sbin/usermod -aG sudo <username>"
+echo "The <username> session must be restart after this action"
