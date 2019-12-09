93d688c8b58a6a8976bdd73007e29dfafb2f2fa0
Alexandre Vingtier <1221867+alxvgt@users.noreply.github.com>
2019-12-09 16:33:16 +0100

    remove specific version

--- install-docker.sh
+++ install-docker.sh
@@ -25,7 +25,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce=${DOCKER_VERSION} docker-ce-cli=${DOCKER_VERSION} containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 echo -e "=== Test ... === \n"
 sudo docker --version
 
