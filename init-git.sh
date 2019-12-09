b60e9f9af8c74f2b8188791022cd02d3ac1e53ad
Alexandre Vingtier <1221867+alxvgt@users.noreply.github.com>
2019-12-09 14:34:01 +0100

    minor adds of git config commands

--- init-git.sh
+++ init-git.sh
@@ -4,3 +4,6 @@
 # apt-get update && apt-get install -y wget && wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-git.sh --no-cache | bash
 
 apt-get install -y git
+
+git config --global user.name "<username>"
+git config --global user.email "<e@mail.com>"
