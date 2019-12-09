9a2ad4c5a432d914ef0476bf94766f73a2304924
Alexandre Vingtier <1221867+alxvgt@users.noreply.github.com>
2019-12-09 09:15:53 +0100

    remove useless locale definiion

--- init-light.sh
+++ init-light.sh
@@ -4,9 +4,9 @@
 # apt-get update && apt-get install -y wget && wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-light.sh --no-cache | bash
 
 apt-get update && apt-get install -y sudo vim git curl wget mlocate netcat net-tools
-apt-get install -y locales && rm -rf /var/lib/apt/lists/* \
-    && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
-LANG=en_US.utf8
+apt-get install -y locales && rm -rf /var/lib/apt/lists/*
+# localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+#LANG=en_US.utf8
 
 echo "To add a user to 'sudo' group, use : /usr/sbin/usermod -aG sudo <username>"
 echo "The <username> session must be restart after this action"
