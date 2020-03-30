dd76ce1f6c7efb0d9e4bae85bf8e998d96ea05f3
Alexandre Vingtier <1221867+alxvgt@users.noreply.github.com>
2020-03-30 19:29:03 +0200

    add zip unzip commands

--- init-light.sh
+++ init-light.sh
@@ -3,7 +3,7 @@
 # to use this file without git clone, use command below (you must be in root)
 # apt-get update && apt-get install -y wget && wget -O - https://raw.githubusercontent.com/alxvgt/init-debian/master/init-light.sh --no-cache | bash
 
-apt-get update && apt-get install -y sudo vim git curl wget mlocate netcat net-tools
+apt-get update && apt-get install -y sudo vim git curl wget mlocate netcat net-tools zip unzip
 apt-get install -y locales && rm -rf /var/lib/apt/lists/*
 # localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 #LANG=en_US.utf8
